8a0cb0d90b1416c5db7e550bf28f975b8d99fef5
DikshaWaghmare <dikshaw44@gmail.com>
2022-02-02 12:00:51 +0530

    added file

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,5 +3,17 @@ function calculate(){
     var height=parseFloat(document.getElementById("height").value);
     var heightM= height/100;
     var bmi=(weight/(heightM * heightM)).toFixed(2);
-    document.getElementById("BMI").innerHTML="BMI is " +bmi;
+    
+    if(bmi < 18.5){
+        document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/>Underweight";
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9){
+        document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/>Normal";
+    }
+    else if(bmi >= 25 && bmi <= 29.9){
+        document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/>Overweight";
+    }
+    else if(bmi >= 30){
+        document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/>Obese";
+    }
 }
