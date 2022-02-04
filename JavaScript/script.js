fb2df3f1ef12382fca8c2fdcc03b0b4f84793153
DikshaWaghmare <dikshaw44@gmail.com>
2022-02-04 14:49:29 +0530

    add progress bar

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,14 +6,18 @@ function calculate(){
     
     if(bmi < 18.5){
         document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/><h4>(Underweight)</h4>";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated";
     }
     else if(bmi >= 18.5 && bmi <= 24.9){
         document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/><h4>(Normal)</h4>";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
     }
     else if(bmi >= 25 && bmi <= 29.9){
         document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/><h4>(Overweight)</h4>";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
     }
     else if(bmi >= 30){
         document.getElementById("BMI").innerHTML="BMI is "+bmi+ "<br/><h4>(Obese)</h4>";
+        document.getElementById("progress-remark").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
     }
 }
